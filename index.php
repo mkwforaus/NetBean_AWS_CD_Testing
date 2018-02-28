9afdb4231d9cf08f8c5810559bd071bc7cd05e62
jasonma <jasonma@192.168.128.182>
2018-02-28 13:12:46 +1000

    testing push

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "<p>" . "Hello Jason and Rudolf." . "</p>";
+        echo "<p>" . "Hello" . "</p>";
         ?>
     </body>
 </html>
